d6e26ba4a90f540705fcb2f0633ec963e4d33ba4
lilbonaf-prog <lilbonaf@gmail.com>
2026-05-02 14:13:27 +0100

    Fix StoreContextProvider import/export mismatch

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom'
-import { StoreContextProvider } from "./context/StoreContext.jsx";
+import StoreContextProvider  from "./context/StoreContext.jsx";
 
 
 createRoot(document.getElementById('root')).render(

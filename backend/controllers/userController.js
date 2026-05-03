2b0c9c254424dfbdb4b43c0b44a90e189040f999
lilbonaf-prog <lilbonaf@gmail.com>
2026-05-03 21:27:57 +0100

    Fix JWT_SECRET and env variables

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -31,6 +31,8 @@ const loginUser = async (req, res) => {
 
 // helper to create JWT
 const createToken = (id) => {
+  console.log("JWT_SECRET value:", process.env.JWT_SECRET);
+
   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "7d" });
 }
 
